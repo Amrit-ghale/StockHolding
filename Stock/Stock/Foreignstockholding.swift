df6f07a31d47b2c88fa4d941aca99cc1473ecd6e
Amrit Ghale <60955689+Amrit-ghale@users.noreply.github.com>
2022-06-04 17:36:31 -0400

    Add files via upload

---
+++ Stock/Stock/Foreignstockholding.swift
@@ -0,0 +1,30 @@
+//
+//  Foreignstockholding.swift
+//  Stock
+//
+//  Created by Amrit Ghale on 03/06/2565 BE.
+//
+
+//import SwiftUI
+import Foundation
+
+ public class ForeignStockHolding: StockHolding
+{
+    var conversionRate: Float = 0.0
+    
+    init(frgnSharePrice: Float, frgnCurrSharePrice: Float, frgnNumOfShare: Int, fgrnCompName: String, conversionRate: Float)
+    {
+        self.conversionRate = conversionRate
+        super.init(sharePrice: frgnSharePrice , currSharePrice: frgnCurrSharePrice, numOfShare: frgnNumOfShare, compName: fgrnCompName)
+    }
+    
+    override public func costInDollars ()-> Float
+    {
+        return purchaseSharePrice * Float(numberOfShares) * conversionRate
+    }
+    
+    override public func valueInDollars ()-> Float
+    {
+        return currentSharePrice * Float(numberOfShares) * conversionRate
+    }
+}
